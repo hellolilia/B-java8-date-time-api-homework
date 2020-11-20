de8c83939137e85ef34ccbdd949281396b4fabbe
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-11-20 13:16:40 +0800

    completed Practice2

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.capability.gtb;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.temporal.WeekFields;
 
 /**
  * 对任意日期获取下一个工作日, 不考虑节假日
@@ -11,6 +13,11 @@ import java.time.LocalDate;
 public class Practice2 {
 
   public static LocalDate getNextWorkDate(LocalDate date) {
-    return null;
+    DayOfWeek dayOfWeek = date.getDayOfWeek();
+    if (dayOfWeek.getValue() == 5) {
+      return date.plusDays(3);
+    } else {
+      return date.plusDays(1);
+    }
   }
 }
